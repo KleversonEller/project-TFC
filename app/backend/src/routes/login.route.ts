908f598bea091921b5ae0d7535024c07a7b68dac
Kleverson Eller <kleverson.a16@gmail.com>
2022-08-20 05:16:57 -0300

    fix: :bug: Corrigindo bug (import biblioteca)
    Corrigindo bug (import biblioteca) onde a biblioteca assync-error nao estava capturando os erros corretamente

--- app/backend/src/routes/login.route.ts
+++ app/backend/src/routes/login.route.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
 import LoginController from '../controllers/login.controller';
+import 'express-async-errors';
 
 const router = express.Router();
 
--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,6 +1,5 @@
 import { App } from './app';
 import 'dotenv/config';
-import 'express-async-errors';
 
 const PORT = process.env.APP_PORT || 3001;
 
