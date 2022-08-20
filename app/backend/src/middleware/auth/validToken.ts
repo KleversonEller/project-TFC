602e7012c5316b134660964143448baae109db61
Kleverson Eller <kleverson.a16@gmail.com>
2022-08-20 06:07:43 -0300

    refactor: :recycle: Refatorando auth (Token.validToken)
    Refatorando auth (Token.validToken) para que valide direto na rota com o express

--- app/backend/src/middleware/auth/validToken.ts
+++ app/backend/src/middleware/auth/validToken.ts
@@ -1,6 +1,9 @@
 import 'dotenv/config';
+import { Response, Request, NextFunction } from 'express';
+import { StatusCodes } from 'http-status-codes';
 import * as jwt from 'jsonwebtoken';
 import UserIn from '../../interfaces/user.interface';
+import ErrorPersonal from '../personal.error';
 
 const secret = process.env.JWT_SECRET || 'lulabolusco';
 
@@ -15,12 +18,16 @@ export default class Token {
     return token;
   }
 
-  static validToken(token: string) {
+  static validToken(req: Request, _res: Response, next: NextFunction) {
     try {
+      const token = req.headers.authorization;
+      if (!token) { throw new ErrorPersonal(StatusCodes.BAD_REQUEST, 'Token notfound'); }
+
       const data = jwt.verify(token, secret);
-      return data;
+      req.body.user = data;
+      return next();
     } catch (error) {
-      throw new Error('Token invalid');
+      throw new ErrorPersonal(StatusCodes.UNAUTHORIZED, 'Invalid Token');
     }
   }
 }
