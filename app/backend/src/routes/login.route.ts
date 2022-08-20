f228e9e70066cb3b24cc0e393663a5c56580855a
Kleverson Eller <kleverson.a16@gmail.com>
2022-08-20 06:11:51 -0300

    feat: :sparkles: Criando route (login/validate)
    Criando route (login/validate) que faz a validação do token ao fazer o login

--- app/backend/src/routes/login.route.ts
+++ app/backend/src/routes/login.route.ts
@@ -1,11 +1,13 @@
 import * as express from 'express';
 import LoginController from '../controllers/login.controller';
+import Token from '../middleware/auth/validToken';
 import 'express-async-errors';
 
 const router = express.Router();
 
 const controller = new LoginController();
 
+router.get('/login/validate', Token.validToken, (req, res) => LoginController.validLogin(req, res));
 router.post('/login', (req, res) => controller.login(req, res));
 
 export default router;
