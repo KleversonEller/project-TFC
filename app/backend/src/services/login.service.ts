46a0910d4bb4b4bc194dea6c5fded3334c5f4837
Kleverson Eller <kleverson.a16@gmail.com>
2022-08-20 05:26:21 -0300

    feat: :sparkles: Criando service (validUserInfo)
    Criando service (validUserInfo) para validar as informaçoes de login do usuario

--- app/backend/src/services/login.service.ts
+++ app/backend/src/services/login.service.ts
@@ -1,6 +1,9 @@
 import { compareSync } from 'bcryptjs';
+import * as Joi from 'joi';
+import { StatusCodes } from 'http-status-codes';
 import Token from '../middleware/auth/validToken';
 import User from '../database/models/user';
+import ErrorPersonal from '../middleware/personal.error';
 
 export default class LoginService {
   constructor(private model = User) {
@@ -8,14 +11,31 @@ export default class LoginService {
   }
 
   public async login(email: string, password: string) {
+    LoginService.validUserInfo(email, password);
+
     const result = await this.model.findOne({ where: { email },
       raw: true });
-    if (!result) { throw new Error('Password or Email invalid'); }
+    if (!result) {
+      throw new ErrorPersonal(StatusCodes.UNAUTHORIZED, 'Incorrect email or password');
+    }
 
     const validPass = compareSync(password, result.password);
-    if (!validPass) { throw new Error('Password or Email invalid'); }
+    if (!validPass) {
+      throw new ErrorPersonal(StatusCodes.UNAUTHORIZED, 'Incorrect email or password');
+    }
 
     const token = Token.newToken(result);
     return token;
+  }
+
+  static validUserInfo(email: string, password: string) {
+    const { error } = Joi.object({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }).validate({ email, password });
+
+    if (error) {
+      throw new ErrorPersonal(StatusCodes.BAD_REQUEST, 'All fields must be filled');
+    }
   }
 }
