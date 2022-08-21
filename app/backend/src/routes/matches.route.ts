fef0cd098d8392f29b802158a59f6a41ce95497a
Kleverson Eller <kleverson.a16@gmail.com>
2022-08-21 20:35:58 -0300

    feat: :sparkles: Criando route (matches/:id/finish)
    Criando route (matches/:id/finish) que altera o status da partida para finalizada

--- app/backend/src/routes/matches.route.ts
+++ app/backend/src/routes/matches.route.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
 import MatchesController from '../controllers/matches.controller';
+import Token from '../middleware/auth/validToken';
 import 'express-async-errors';
 
 const router = express.Router();
@@ -7,6 +8,7 @@ const router = express.Router();
 const controller = new MatchesController();
 
 router.get('/matches', (req, res) => controller.getAll(req, res));
-router.post('/matches', (req, res) => controller.create(req, res));
+router.post('/matches', Token.validToken, (req, res) => controller.create(req, res));
+router.patch('/matches/:id/finish', (req, res) => controller.update(req, res));
 
 export default router;
