6bcd284bea5f7bdb8674f78838c5f91cbf736b8a
Kleverson Eller <kleverson.a16@gmail.com>
2022-08-21 20:34:00 -0300

    feat: :sparkles: Criando service matches (update)
    Criando service matches (update) que atualiza uma partida para finalizada

--- app/backend/src/services/matches.service.ts
+++ app/backend/src/services/matches.service.ts
@@ -1,5 +1,6 @@
 import Matches from '../database/models/matches';
 import Team from '../database/models/team';
+import MatchesIn from '../interfaces/matches.interface';
 
 export default class MatchesService {
   constructor(private model = Matches) {
@@ -14,11 +15,16 @@ export default class MatchesService {
     return result;
   }
 
-  public async create(data: any) {
+  public async create(data: MatchesIn) {
     const { homeTeam, awayTeam, homeTeamGoals, awayTeamGoals } = data;
     const result = await this.model.create(
       { homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, inProgress: true },
     );
     return result;
+  }
+
+  public async update(id: string) {
+    await this.model.update({ inProgress: false }, { where: { id } });
+    return { message: 'Finished' };
   }
 }
