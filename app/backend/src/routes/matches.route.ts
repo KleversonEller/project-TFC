f04e524039d515eed8ac7b5a843c7fbfd4b1ff7b
Kleverson Eller <kleverson.a16@gmail.com>
2022-08-21 19:55:20 -0300

    feat: :sparkles: Criando route POST (matches)
    Criando route POST (matches) responsavel por inserir um novo matche no banco

--- app/backend/src/routes/matches.route.ts
+++ app/backend/src/routes/matches.route.ts
@@ -7,5 +7,6 @@ const router = express.Router();
 const controller = new MatchesController();
 
 router.get('/matches', (req, res) => controller.getAll(req, res));
+router.post('/matches', (req, res) => controller.create(req, res));
 
 export default router;
