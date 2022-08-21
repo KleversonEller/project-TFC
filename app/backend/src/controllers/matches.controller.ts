6334cefa48d7d45b14a8e9bfa283a62b58e19ca5
Kleverson Eller <kleverson.a16@gmail.com>
2022-08-21 01:59:20 -0300

    feat: :sparkles: Criando controller (matches)
    Criando controller (matches) com a função getAll que retorna a response para a rota matches

---
+++ app/backend/src/controllers/matches.controller.ts
@@ -0,0 +1,14 @@
+import { Response, Request } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import MatchesService from '../services/matches.service';
+
+export default class MatchesController {
+  constructor(private service = new MatchesService()) {
+    this.service = service;
+  }
+
+  public async getAll(req: Request, res: Response) {
+    const result = await this.service.getAll();
+    return res.status(StatusCodes.OK).json(result);
+  }
+}
