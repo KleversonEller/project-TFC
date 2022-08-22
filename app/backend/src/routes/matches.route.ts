ff69e05e2ff454140bd1b50bdc3e6ebf60c67826
Kleverson Eller <kleverson.a16@gmail.com>
2022-08-21 21:44:59 -0300

    feat: :sparkles: Criando route PATCH (matches/:id)
    Criando route PATCH (matches/:id) que atualiza o placar de uma partida

--- app/backend/src/routes/matches.route.ts
+++ app/backend/src/routes/matches.route.ts
@@ -10,5 +10,6 @@ const controller = new MatchesController();
 router.get('/matches', (req, res) => controller.getAll(req, res));
 router.post('/matches', Token.validToken, (req, res) => controller.create(req, res));
 router.patch('/matches/:id/finish', (req, res) => controller.update(req, res));
+router.patch('/matches/:id', (req, res) => controller.updateScoreboard(req, res));
 
 export default router;
