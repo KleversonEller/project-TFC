32d1a6087528add9ca33b1534c114f84a83d5f76
Kleverson Eller <kleverson.a16@gmail.com>
2022-08-23 23:13:15 -0300

    feat: :sparkles: Criando interfaces (game, table, team)

---
+++ app/backend/src/interfaces/game.interface.ts
@@ -0,0 +1,4 @@
+export default interface GameIn {
+  homeTeamGoals: number
+  awayTeamGoals: number
+}
---
+++ app/backend/src/interfaces/team.interface.ts
@@ -0,0 +1,5 @@
+export default interface TeamIn {
+  teamName: string
+  homeTeam: []
+  awayTeam: []
+}
